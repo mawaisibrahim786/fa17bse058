1a979def276ca4aa51639e5c6c9bb10dd1957d2a
Awais Ibrahim <maharawais348@gmail.com>
2020-12-05 15:38:07 +0500

    tow level

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: 'Awesome App',
-    home: HomePage(),
-    theme: ThemeData(
-        primarySwatch: Colors.purple
-    ),
-
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
   ));
 }
 
-class  HomePage extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('M Awais Ibrahim  ' 'FA17-BSE-0058'),
+        image: new Image.asset('image.jpeg'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.red);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Awesome App',
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -34,32 +60,53 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
               children: [
-                Image.asset(
-                  'images/xyz.jpg',fit: BoxFit.cover,
-                ),
-                SizedBox(height: 20,),
-                Text(myText,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter Something Here',
-                        labelText: 'Name'
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text(
+                        'Simple',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        debugPrint('simple');
+                      },
                     ),
-
-
                   ),
-                )
-
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text(
+                        'Hard',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        debugPrint('hard');
+                      },
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
+          ],
         ),
       ),
       drawer: Drawer(
@@ -70,10 +117,7 @@ class _HomePageState extends State<HomePage> {
                 accountName: Text('M Awais Ibrahim'),
                 accountEmail: Text('maharawais348@gmail.com'),
                 currentAccountPicture: CircleAvatar(
-                    backgroundImage: AssetImage('images/abc.jpg')
-
-                )
-            ),
+                    backgroundImage: AssetImage('images/abc.jpg'))),
             ListTile(
               leading: Icon(Icons.person),
               title: Text('Account'),
@@ -95,13 +139,11 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.refresh),
-      ),
     );
+  }
+  void hardDice() {
+    setState(() {
+      Navigator.push(context, new MaterialPageRoute(builder: (context) => new HardDice()));
+    });
   }
 }
